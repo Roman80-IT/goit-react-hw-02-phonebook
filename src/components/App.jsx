baca2c035cb285cd0e2a7d141266eb4a494aa35f
Roman <jrj80mail@gmail.com>
2023-10-02 13:54:01 +0300

    step-3 b
    + жорстке закодування початкового стану з певними даними

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ export class ContactList extends Component {
 
     return (
       <div>
-        <h2>Контакти:</h2>
+        <h2>Contacts:</h2>
         <ul>
           {contacts.map(contact => (
             <li key={contact.id}>
@@ -23,7 +23,13 @@ export class ContactList extends Component {
 
 export class App extends Component {
   state = {
-    contacts: [],
+    //   contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
     name: '',
     number: '',
