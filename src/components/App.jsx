4eb673e816e106ddb345b424acc849a71406c6c6
Roman <jrj80mail@gmail.com>
2023-10-02 11:23:52 +0300

    readme & install nanoid

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +10,17 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
     </div>
   );
 };
+
+const id1 = nanoid();
+const id2 = nanoid();
+const id3 = nanoid();
+const id4 = nanoid();
+
+console.log(id1, id2, id3, id4);
