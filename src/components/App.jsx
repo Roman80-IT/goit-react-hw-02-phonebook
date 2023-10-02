6ad099af5424266b8c16751e6c21813f35c6c1aa
Roman <jrj80mail@gmail.com>
2023-10-02 11:32:24 +0300

    step-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,67 @@
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+export class ContactList extends Component {
+  render() {
+    const { contacts } = this.props;
 
-const id1 = nanoid();
-const id2 = nanoid();
-const id3 = nanoid();
-const id4 = nanoid();
+    return (
+      <div>
+        <h2>Контакти:</h2>
+        <ul>
+          {contacts.map(contact => (
+            <li key={contact.id}>{contact.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
-console.log(id1, id2, id3, id4);
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleNameChange = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleAddContact = () => {
+    const { name, contacts } = this.state;
+    if (name.trim() === '') {
+      return; // Не додавати порожні контакти
+    }
+
+    const newContact = {
+      name,
+      id: nanoid(), // Генеруємо унікальний ідентифікатор
+    };
+
+    this.setState({
+      contacts: [...contacts, newContact],
+      name: '', // Очищуємо поле імені після додавання контакту
+    });
+  };
+
+  render() {
+    const { contacts, name } = this.state;
+
+    return (
+      <div>
+        <h1>Телефонна книга</h1>
+        <input
+          type="text"
+          name="name"
+          required
+          value={name}
+          onChange={this.handleNameChange}
+          placeholder="Введіть ім'я контакту"
+        />
+        <button onClick={this.handleAddContact}>Додати контакт</button>
+        <ContactList contacts={contacts} />
+      </div>
+    );
+  }
+}
